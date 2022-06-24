635ea993a940ed990de838005ef9bab2091e0c14
SilvVF <david.silver232@gmail.com>
2022-06-23 22:35:35 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Home } from './containers/Home';
+
+
+const Stack = cre
+
+// will be changed how does this work i have no idea ... = varargs maybe im lost ? 
+const theme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    card: "rgb(1, 10, 19)",
+    background: "rgb(37, 45, 50)",
+    primary: "rgb(255, 255, 255)",
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: 'Welcome' }}
+          />
+      </Stack.Navigator>
+    
+
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/Card.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+
+const card = ({}) => {
+    
+
+}
+
+export {card}
